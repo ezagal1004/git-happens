4ab76ad62a9c5f98b5a69c4e6aa6c9c8f05140e4
ezagal2 <125419919+ezagal2@users.noreply.github.com>
2025-08-09 15:04:31 -0700

    shhhhhhh

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${pressStart2P.variable} ${vt323.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
